ca5aa5bbe3527286f3b674fbf9200aac81b72c84
Yohai Wiener <yohaiwiener@gmail.com>
2025-01-01 11:31:10 +0200

    feature: add loading state to pvp game lobby

--- Views/InviteGameView/LobbyView.tsx
+++ Views/InviteGameView/LobbyView.tsx
@@ -2,15 +2,20 @@ import { ThemedText } from '@/components/ThemedText'
 import DangerButton from '@/components/ui/buttons/DangerButton'
 import IconButton from '@/components/ui/buttons/IconButton'
 import ShareIcon from '@/components/ui/icons/ShareIcon'
+import Loader from '@/components/ui/Loader'
 import Screen from '@/components/ui/screen-template/Screen'
+import { Colors } from '@/constants/Colors'
 import { ClientMessageType, LeaveInviteGameMessage } from '@/server/messageTypes'
 import { useGlobalStore } from '@/stores/globalStore'
+import { isShader } from '@shopify/react-native-skia'
 import { router } from 'expo-router'
 import React from 'react'
-import { Share } from 'react-native'
+import { Share, useColorScheme, View } from 'react-native'
 
 export default function LobbyView() {
   const { gameID, ws } = useGlobalStore()
+  const theme = useColorScheme() ?? 'light'
+  const [hasShared, setHasShared] = React.useState(false)
   // const baseUrl = 'strixos://invite-game/'
   const baseUrl = 'https://play.strixos.app/invite-game/'
 
@@ -24,6 +29,8 @@ export default function LobbyView() {
       title: 'Join my game',
       url: baseUrl + gameID,
       message: 'Join my game: ' + baseUrl + gameID,
+    }).then(() => {
+      setHasShared(true)
     })
   }
 
@@ -42,10 +49,19 @@ export default function LobbyView() {
     <Screen withRouteHeader>
       <Screen.Header />
       <Screen.Body>
-        <IconButton onPress={shareGame}>
-          <ShareIcon />
-        </IconButton>
-        <ThemedText type="subtitle">Share game link</ThemedText>
+        {hasShared ? (
+          <View style={{ justifyContent: 'center', alignItems: 'center', gap: 20 }}>
+            <Loader color={Colors[theme].primary} />
+            <ThemedText type="subtitle">Waiting for your friend to join...</ThemedText>
+          </View>
+        ) : (
+          <>
+            <IconButton onPress={shareGame}>
+              <ShareIcon />
+            </IconButton>
+            <ThemedText type="subtitle">Share game link</ThemedText>
+          </>
+        )}
       </Screen.Body>
       <Screen.Footer>
         <DangerButton label="Cancel" onPress={leaveInviteGame} />
