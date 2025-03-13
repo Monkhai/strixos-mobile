fa59310ed6a14b88df2c5b20ecf3afb0bdca3ac2
Yohai Wiener <yohaiwiener@gmail.com>
2025-03-13 10:33:04 +0200

    fix: production server url

--- server/constants.ts
+++ server/constants.ts
@@ -1,11 +1,5 @@
-// localhost server
-export let WS_URL = 'ws://10.100.102.149:8080/ws'
+// // localhost server
+// export const WS_URL = 'ws://10.100.102.149:8080/ws'
 
-// if env is production, set WS_URL to production server
-if (process.env.NODE_ENV === 'production') {
-  console.log('production')
-  WS_URL = 'wss://server.strixos.app/ws'
-}
-
-// // production server
-// export const WS_URL = 'wss://server.strixos.app/ws'
+// production server
+export const WS_URL = 'wss://server.strixos.app/ws'
